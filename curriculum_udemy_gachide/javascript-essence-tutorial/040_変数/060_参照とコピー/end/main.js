860d9bc28a0197d138444c48e6383124abe625bc
hioken <radcloseff@gmail.com>
2025-01-27 03:27:26 +0900

    fin c4

--- curriculum_udemy_gachide/javascript-essence-tutorial/040_変数/060_参照とコピー/end/main.js
+++ curriculum_udemy_gachide/javascript-essence-tutorial/040_変数/060_参照とコピー/end/main.js
@@ -1,12 +1,33 @@
-let a = 'hello';
-let b = a;
-b = 'bye';
-console.log(a, b);
+// let a = 'hello';
+// let b = a;
+// b = 'bye';
+// console.log(a, b);
 
-let c = {
-    prop: 'hello'
+// let c = {
+//     prop: 'hello'
+// }
+// let d = c;
+// // d.prop = 'bye';
+// d = {};
+// console.log(c, d);
+
+
+let p = 1;
+let o1 = {
+    prop: 'a'
+};
+let o2 = {
+    prop: 'a'
+};
+
+function fc(v, obj1, obj2) {
+    v = 200;
+    obj1.prop = 'abc';
+    obj2 = 'abc';
 }
-let d = c;
-// d.prop = 'bye';
-d = {};
-console.log(c, d);
+
+fc(p, o1, o2)+
+
+console.log(p);
+console.log(o1);
+console.log(o2);
--- curriculum_udemy_gachide/javascript-essence-tutorial/040_変数/090_参照と分割代入/end/main.js
+++ curriculum_udemy_gachide/javascript-essence-tutorial/040_変数/090_参照と分割代入/end/main.js
@@ -1,28 +1,49 @@
-const a = {
-    prop: 0
-}
+// const a = {
+//     prop: 0
+// }
 
-let { prop } = a;
+// let { prop } = a;
+// prop = 1;
+// console.log(a, prop);
 
-prop = 1;
+// let b = a.prop;
+// console.log(b);
+// b = 1;
+// console.log(b);
+// console.log(a);
 
-console.log(a, prop);
 
-function fn({ prop }) {
-    prop = 1;
-    console.log(a, prop);
-}
 
-fn(a);
 
-const c = {
-    prop1: {
-        prop2: 0
-    }
-}
 
-let { prop1 } = c;
+// function fn({ prop }) {
+//     prop = 1;
+//     console.log(a, prop);
+// }
 
-prop1.prop2 = 1;
+// fn(a);
+// const c = {
+//     prop1: {
+//         prop2: 0
+//     }
+// }
+// let { prop1 } = c;
+// prop1.prop2 = 1;
+// console.log(c, prop1);
 
-console.log(c, prop1);
+
+
+
+// const obj = { a: { b: { c: 42 } } };
+
+// const { a: { b: { c } } } = obj;
+// const d = obj.a.b.c;
+// console.log(c); // 42
+// console.log(d); // 42
+// console.log(a); // error
+
+const a = {prop: [1, 2]}
+let [b, c] = a.prop;
+
+console.log(b); 
+console.log(c); 
